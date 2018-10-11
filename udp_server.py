52f1cbcd44a74213d640603ee7b82df5cfccf90b
xiaochen <xcwang821@gmail.com>
2018-10-11 18:37:00 -0400

    print instead of log

--- udp_server.py
+++ udp_server.py
@@ -1,9 +1,7 @@
-import logging
 import socket
 import sys
 from util import *
 
-logger = logging.getLogger()
 addresses = []
 
 
@@ -13,17 +11,16 @@ def main(host="", port=7000):
     sock.bind((host, port))
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        logger.info("connection from: %s", addr)
+        print("connection from: %s", addr)
         addresses.append(addr)
         if len(addresses) >= 2:
-            logger.info("server - send client info to: %s", addresses[0])
+            print("server - send client info to: %s", addresses[0])
             sock.sendto(addr_to_msg(addresses[1]), addresses[0])
-            logger.info("server - send client info to: %s", addresses[0])
+            print("server - send client info to: %s", addresses[0])
             sock.sendto(addr_to_msg(addresses[0]), addresses[1])
             addresses.pop(1)
             addresses.pop(0)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
     main(*addr_from_args(sys.argv))
